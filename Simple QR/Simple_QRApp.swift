816119b91ada6c2d0a53b2cce639eccb088f45d7
reallyShould <77869589+reallyShould@users.noreply.github.com>
2026-07-17 09:19:09 +0700

    Initial Commit

---
+++ Simple QR/Simple_QRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_QRApp.swift
+//  Simple QR
+//
+//  Created by Никита on 17.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Simple_QRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
